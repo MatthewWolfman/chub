fd74be7f3f00f42639ca2b10b3560f44e0caf630
Matthew Wolfman <Phelix@initiateid.com>
2016-01-03 02:56:49 -0500

    Trying to get button to display text underneath with AJAX and failing.

--- login.js
+++ login.js
@@ -1,9 +1,10 @@
 function loadDoc() {
-	var xhttp = new XMLHttpRequest();
-	xhttp.open("GET", "reel/01032016.txt", true);
-	xhttp.onreadystatechange = function() {
-		if (xhttp.readyState == 4 && xhttp.status == 200) {
-		  document.getElementById("txt").innerHTML = xhttp.responseText;
-		}
-	}
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+      document.getElementById("demo").innerHTML = xhttp.responseText;
+    }
+  };
+  xhttp.open("GET", "reel/test.txt", true);
+  xhttp.send();
 }
