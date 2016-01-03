3464c25e7ae1a90822aca2d8186259860ae85534
Matthew Wolfman <Phelix@initiateid.com>
2016-01-03 03:23:36 -0500

    Made gameDatabase.html look nice and consistent with login.html. Also put a redirect from login to inde.html.

--- login.js
+++ login.js
@@ -1,10 +1,14 @@
-function loadDoc() {
+function showOne() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (xhttp.readyState == 4 && xhttp.status == 200) {
-      document.getElementById("demo").innerHTML = xhttp.responseText;
+      document.getElementById("text1").innerHTML = xhttp.responseText;
     }
   };
   xhttp.open("GET", "reel/test.txt", true);
   xhttp.send();
+}
+
+function success() {
+	location.href = "index.html";
 }
