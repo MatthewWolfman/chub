486dd7424884222b0d50378657517c31dfb262d1
Matthew Wolfman <Phelix@initiateid.com>
2016-01-05 01:21:31 -0500

    "Added Login fail for now to make sure people dont spam the login. I also made a myGames tab.

--- login.js
+++ login.js
@@ -1,3 +1,29 @@
+var tries = {
+	attempts:0
+};
+
+function success() {
+	//Will be a database look up, nut for now I will be using variables
+	var testUser = "p";
+	var testPass = "p";
+	
+	var u = document.getElementById("user").value;
+	var p = document.getElementById("pass").value;
+	
+	if (u === testUser && p === testPass){
+		location.href = "index.html";
+	} else {
+		if(tries.attempts < 5){
+			tries.attempts += 1;
+			document.getElementById("wrongPass").innerHTML = "Wrong username and password!";
+		} else {
+			document.getElementById("wrongPass").innerHTML = "Exceeded tries, wait 10 seconds!";
+			tries.attempts = 0;
+			location.href = "loginFail.html";
+		}
+	}
+}
+
 function showOne() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
@@ -7,8 +33,4 @@ function showOne() {
   };
   xhttp.open("GET", "reel/test.txt", true);
   xhttp.send();
-}
-
-function success() {
-	location.href = "index.html";
 }
