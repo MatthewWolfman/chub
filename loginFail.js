f63caecb078d7546cbe1e0391ffb40e9d3177ee2
Matthew Wolfman <Phelix@initiateid.com>
2016-01-05 14:05:18 -0500

    Started adding php.

--- loginFail.js
+++ loginFail.js
@@ -44,3 +44,14 @@ function myTimeout2() {
 function myTimeout1() {
     document.getElementById("time").innerHTML = "10 seconds";
 }
+
+function showOne() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+      document.getElementById("text1").innerHTML = xhttp.responseText;
+    }
+  };
+  xhttp.open("GET", "reel/test.txt", true);
+  xhttp.send();
+}
