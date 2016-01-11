7c78cc69f3bd21f4abed086d9648e7118072bcdc
Phelix <Phelix@initateid.org>
2016-01-10 23:59:47 -0500

    Login is based off the database.

--- checkUser.php
+++ checkUser.php
@@ -12,26 +12,23 @@
 		$dbc = mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) OR die ('Could not connect to the MySQL: ' . mysql_error() );
 		mysql_select_db (DB_NAME, $dbc) OR die ('Could not select the database: ' . mysql_error() );
 		
-		$sql1 = mysql_query("SELECT * FROM login WHERE username = '$user' AND password = '$pass'");
+		$sql = mysql_query("SELECT * FROM login WHERE username = '$user' AND password = '$pass'");
 		$row = array();
 		
-		while($r = mysql_fetch_assoc($sql1)) {
+		while($r = mysql_fetch_assoc($sql)) {
 			$row["user"] = $r;
 		}
 		
 		if(empty($row)){
-			echo "Wrong user name";
+			echo "Wrong user name or password!";
 		} else {
 			$_SESSION["user"] = $row["user"]["username"];
-			print json_encode($row);
+			header('Location: index.html');
 		}
 	}
-	
-	//$user = $_POST["user"]; 
-	//$pass = $_POST["pass"]; 
-		
-	$user = "p";
-	$pass = "p";
+
+	$user = $_POST['user'];
+	$pass = $_POST['pass'];
 	
 	checkUser($user, $pass);
 ?>
